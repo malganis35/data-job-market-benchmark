a9429266f039228a1acde17f87b25fb4b723e52f
Cao Tri DO <caotri.do88@gmail.com>
2026-05-23 21:48:59 +0200

    fix: correct import from kaggle

--- modules/importer.py
+++ modules/importer.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
 
 class DataImport:
     """"
@@ -9,9 +11,12 @@ class DataImport:
         pass
 
     @st.cache_data(ttl=60*60) # ttl of one hour to keep memory in cache
-    def fetch_and_clean_data():
-        data_url = 'https://storage.googleapis.com/gsearch_share/gsearch_jobs.csv'
-        jobs_data = pd.read_csv(data_url).replace("'","", regex=True)
+    def fetch_and_clean_data(_self):
+        jobs_data = kagglehub.load_dataset(
+            KaggleDatasetAdapter.PANDAS,
+            "lukebarousse/data-analyst-job-postings-google-search",
+            "gsearch_jobs.csv",
+        ).replace("'","", regex=True)
         jobs_data.date_time = pd.to_datetime(jobs_data.date_time)
         jobs_data = jobs_data.drop(labels=['Unnamed: 0', 'index'], axis=1, errors='ignore')
         jobs_data.description_tokens = jobs_data.description_tokens.str.strip("[]").str.split(",") # fix major formatting issues with tokens
